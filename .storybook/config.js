9543cf767eae0e43d5cf55403e0a15f71f0b3bf2
oscar.bellon <oscar.bellon@odigeo.com>
2019-07-09 16:43:36 +0200

    Add viewport addon and customize display

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,15 @@
-import { configure } from '@storybook/html';
+import { configure, addParameters } from '@storybook/html';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import odfTheme from './odfTheme.js';
+
+addParameters({ 
+  viewport: { 
+    viewports: INITIAL_VIEWPORTS,
+  },
+  options: {
+    theme: odfTheme,
+  }
+});
 
 function loadStories() {
   require('../stories/button.stories');
